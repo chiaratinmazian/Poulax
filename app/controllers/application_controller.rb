92b74b12a4445d6a97028367e263ed2ddfbb85a5
chiara tinmazian <chiara.tinmazian@gmail.com>
2019-11-25 17:45:24 +0100

    devise setup done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  def configure_permitted_parameters
+      # For additional in app/views/devise/registrations/edit.html.erb
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
