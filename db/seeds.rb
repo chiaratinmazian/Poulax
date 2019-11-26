c6c5de50b37cd133acab07303b75cc5c57bc6853
sandra moutoussamy <sandra.moutous@gmail.com>
2019-11-26 15:50:55 +0100

    controller-hens created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts "Cleaning database..."
+Hen.destroy_all
+User.destroy_all
+Reservation.destroy_all
+
+puts "Creating data..."
+
+20.times do
+  user = User.create!(
+    username: Faker::Internet.username,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 6)
+    )
+  cities = ["Montauban", "Marseille", "Saint-Martin de Ré", "Dijon", "Arcachon", "Biscarrosse", "Lille", "Le Mans", "Strasbourg", "Annecy", "La Roche Sur Yon", "La Rochelle"]
+  8.times do
+    url = "https://source.unsplash.com/collection/8666324"
+    hen = Hen.create!(
+      name: Faker::Name.female_first_name,
+      description: Faker::Lorem.paragraph,
+      city: cities.sample,
+      eggs_per_day: Faker::Number.within(range:1..10),
+      price_per_day: Faker::Number.within(range:5..20),
+      photo: url,
+      user: user
+      )
+    end
+  resa = Reservation.create!(
+    start_date: "2019-11-30",
+    end_date: "2019-12-05",
+    user: User.all.sample,
+    hen: user.hens.first
+    )
+  end
+
+puts "Database created !!!"
+
+
+
+
