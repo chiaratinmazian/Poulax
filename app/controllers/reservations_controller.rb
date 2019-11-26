9bb50466cd9840bd04037cf5da44b255b2afa276
sandra moutoussamy <sandra.moutous@gmail.com>
2019-11-26 18:29:59 +0100

    reservations action new and create ok

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,26 @@
 class ReservationsController < ApplicationController
+  before_action :set_hen
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.hen = @hen
+    if @reservation.save
+      redirect_to hen_path(@hen)
+    else
+      render 'hens/show'
+    end
+  end
+
+  private
+
+  def set_hen
+    @hen = Hen.find(params[:hen_id])
+  end
+
+  def reservation_params
+    params.require(:reservations).permit(:start_date, :end_date, :user_id)
+  end
 end
