7e2d51480728233bc882ade157ae2ce7d74884ea
sandra moutoussamy <sandra.moutous@gmail.com>
2019-11-27 12:37:41 +0100

    comm

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :hens, only: [:index, :show] do
     resources :reservations, only: [:new, :create]
+    collection do
+      get 'dashboard'
+    end
   end
 end
