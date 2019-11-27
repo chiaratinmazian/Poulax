5e167146032977297815c899d994731a8ed36a17
sandra moutoussamy <sandra.moutous@gmail.com>
2019-11-27 14:21:31 +0100

    dashboard route created

--- app/controllers/hens_controller.rb
+++ app/controllers/hens_controller.rb
@@ -6,4 +6,23 @@ class HensController < ApplicationController
   def show
     @hen = Hen.find(params[:id])
   end
+
+  def new
+    @hen = Hen.new
+  end
+
+  def create
+    @hen = Hen.new(hen_params)
+    if @hen.save
+      redirect_to 'dashboard'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def hen_params
+    params.require(:hen).permit(:name, :city, :description, :eggs_per_day, :price_per_day, :photo)
+  end
 end
