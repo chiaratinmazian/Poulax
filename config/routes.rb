88141f9492644f49c6d12e944dce210d8942b3ed
sandra moutoussamy <sandra.moutous@gmail.com>
2019-11-27 13:21:50 +0100

    dashboard page added

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :hens, only: [:index, :show] do
     resources :reservations, only: [:new, :create]
-    collection do
-      get 'dashboard'
-    end
   end
+
+  get 'dashboard', to: 'pages#dashboard'
 end
