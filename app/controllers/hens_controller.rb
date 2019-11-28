eecb0236526198cdc68a5d3187c55ad7f2fb9c9c
chiara tinmazian <chiara.tinmazian@gmail.com>
2019-11-28 12:55:18 +0100

    made changes to dry the code

--- app/controllers/hens_controller.rb
+++ app/controllers/hens_controller.rb
@@ -1,4 +1,6 @@
 class HensController < ApplicationController
+  before_action :set_hen, only: [:show, :edit, :update, :destroy]
+
   def index
     @hens = Hen.all
     @hens_map = Hen.geocoded
@@ -11,9 +13,7 @@ class HensController < ApplicationController
     end
   end
 
-  def show
-    @hen = Hen.find(params[:id])
-  end
+  def show; end
 
   def new
     @hen = Hen.new
@@ -29,12 +29,9 @@ class HensController < ApplicationController
     end
   end
 
-  def edit
-    @hen = Hen.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @hen = Hen.find(params[:id])
     if @hen.update(hen_params)
       redirect_to hen_path(@hen)
     else
@@ -43,11 +40,14 @@ class HensController < ApplicationController
   end
 
   def destroy
-    @hen = Hen.find(params[:id])
     @hen.destroy
   end
 
   private
+
+  def set_hen
+    @hen = Hen.find(params[:id])
+  end
 
   def hen_params
     params.require(:hen).permit(:name, :city, :description, :eggs_per_day, :price_per_day, :photo)
