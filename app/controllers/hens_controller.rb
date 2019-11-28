613a3b60319872ab85aab2583b1fda33596dd4fe
chiara tinmazian <chiara.tinmazian@gmail.com>
2019-11-28 17:34:35 +0100

    added the searchbar

--- app/controllers/hens_controller.rb
+++ app/controllers/hens_controller.rb
@@ -2,10 +2,11 @@ class HensController < ApplicationController
   before_action :set_hen, only: [:show, :edit, :update, :destroy]
 
   def index
-    if params[:query].present?
+    if Hen.geocoded.where("address ILIKE ?", "%#{params[:query]}%").length > 0
       @hens = Hen.geocoded.where("address ILIKE ?", "%#{params[:query]}%") # ("title ILIKE ?", "%#{params[:query]}%")
     else
-      @hens = Hen.near(params[:query], 10)
+      @hens = Hen.all
+      flash.now[:notice] = "Pas de poules à l'adresse indiquée, vous êtes revenu à l'index !"
     end
     @markers = @hens.map do |hen|
       {
